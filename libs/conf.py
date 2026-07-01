c25ed7d96f093ff65fdd2712f5ec51c96bb117e3
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-07-01 20:21:09 +0000

    Increase Camera Resolution

--- libs/conf.py
+++ libs/conf.py
@@ -2,7 +2,7 @@ import os
 
 def get_conf():
     config = {
-        "CAMERA_RESOLUTION": (640, 480),
+        "CAMERA_RESOLUTION": (1280, 720),
         "OUTPUT_RESOLUTION": (640, 480),
         "MODEL_PATH": f"{os.getcwd()}/libs/models/movenet_lightning_int8.tflite"
     }
