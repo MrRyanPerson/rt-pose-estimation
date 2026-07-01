493139778a22aa485d0f455439b54308bf33b190
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-07-01 17:36:58 +0000

    Upload ML model

---
+++ libs/conf.py
@@ -0,0 +1,9 @@
+import os
+
+def get_conf():
+    config = {
+        "CAMERA_RESOLUTION": (640, 480),
+        "OUTPUT_RESOLUTION": (640, 480),
+        "MODEL_PATH": f"{os.getcwd()}/libs/models/movenet_lightning_f16.tflite"
+    }
+    return config
--- libs/pose_estimation.py
+++ libs/pose_estimation.py
@@ -1,1 +1,13 @@
-from libs.libraries import PiCamera2, Interpreter, cv2
+from libs.libraries import load_dependencies
+
+PiCamera2, Interpreter, cv2 = load_dependencies()
+
+class PoseEstimator:
+    def __init__(self, conf):
+        self.conf = conf
+        self.interpreter = Interpreter(model_path=conf["MODEL_PATH"], num_threads=4)
+        self.output_resolution = conf["OUTPUT_RESOLUTION"]
+
+    def preprocess(self, frame):
+        
+        
