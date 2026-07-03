9ad9ec0b46acba1c76f928df966e62da5508344f
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-07-03 19:54:53 +0000

    Add multiple camera backends for csi camera & usb camera

--- libs/capture.py
+++ libs/capture.py
@@ -1,4 +1,5 @@
 import time
+from unittest import case
 from libs.libraries import load_dependencies
 
 PiCamera2, Interpreter, cv2, numpy, logger = load_dependencies()
@@ -6,16 +7,27 @@ PiCamera2, Interpreter, cv2, numpy, logger = load_dependencies()
 class PiCamera:
     def __init__(self, conf):
         self.conf = conf
-        self.picam2 = PiCamera2()
-        capture_config = self.picam2.create_still_configuration({"size": self.conf["CAMERA_RESOLUTION"]})
-        self.picam2.configure(capture_config)
-        self.picam2.start()
-        time.sleep(2)  
-        logger.info("Camera initialized")
+        match conf["CAMERA_BACKEND"]:
+            case "PiCamera2":
+                self.picam2 = PiCamera2()
+                capture_config = self.picam2.create_still_configuration({"size": self.conf["CAMERA_RESOLUTION"]})
+                self.picam2.configure(capture_config)
+                self.picam2.start()
+                time.sleep(2)  
+                logger.info("Raspberry Pi Camera initialized")
+            case "OpenCV":
+                self.cap = cv2.VideoCapture(conf["CAMERA_PORT"])
+                self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, conf["CAMERA_RESOLUTION"][0])
+                self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, conf["CAMERA_RESOLUTION"][1])
+                logger.info("OpenCV Camera initialized")
+            
 
     def capture_frame(self):
-        frame = self.picam2.capture_array()
-        
+        match self.conf["CAMERA_BACKEND"]:
+            case "PiCamera2":
+                frame = self.picam2.capture_array()
+            case "OpenCV":
+                ret, frame = self.cap.read()
         return frame
 
 
--- libs/conf.py
+++ libs/conf.py
@@ -2,6 +2,8 @@ import os
 
 def get_conf():
     config = {
+        "CAMERA_BACKEND": "PiCamera2", # Can be PiCamera2 or OpenCV
+        "CAMERA_PORT": 0, # index of camera, can be blank if using PiCamera2
         "CAMERA_RESOLUTION": (1280, 720),
         "OUTPUT_RESOLUTION": (640, 480),
         "MODEL_PATH": f"{os.getcwd()}/libs/models/movenet_lightning_int8.tflite"
